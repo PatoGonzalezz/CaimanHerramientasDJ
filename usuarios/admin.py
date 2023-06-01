ec9c91b74cfd0aec1fefdf9d346f2181535d4d69
PatoGonzalezz <pato12lc@gmail.com>
2023-06-01 19:04:41 -0400

    Se hace BD

--- usuarios/admin.py
+++ usuarios/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
+from .models import herramientaEl,herramientaIn, accesorio
 # Register your models here.
+
+
+admin.site.register(herramientaEl)
+admin.site.register(herramientaIn)
+admin.site.register(accesorio)
+
--- usuarios/models.py
+++ usuarios/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
 # Create your models here.
+
+class herramientaEl(models.Model):
+    id_herraEl = models.CharField(primary_key = True   , max_length= 10)
+    nombre_herraEl  = models.CharField(max_length=20)
+    precio_herraEl  = models.CharField(max_length=10000000)
+
+    def __str__(self):
+        return str(self.id_herraEl ) + " " +str(self.nombre_herraEl)
+
+class herramientaIn(models.Model):
+    id_herraIn = models.CharField(primary_key = True   , max_length= 10)
+    nombre_herraIn  = models.CharField(max_length=20)
+    precio_herraIn  = models.CharField(max_length=10000000)
+
+    def __str__(self):
+        return str(self.id_herraIn ) + " " +str(self.nombre_herraIn)
+
+class accesorio(models.Model):
+    id_accesorio = models.CharField(primary_key = True   , max_length= 10)
+    nombre_accesorio  = models.CharField(max_length=20)
+    precio_accesorio  = models.CharField(max_length=10000000)
+
+    def __str__(self):
+        return str(self.id_accesorio ) + " " +str(self.nombre_accesorio)
